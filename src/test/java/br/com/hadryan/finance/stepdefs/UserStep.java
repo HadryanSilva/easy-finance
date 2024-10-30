ef44fb37ada4405351fcc9d521db7fc22a72adbf
Hadryan Silva <hadryan2001@gmail.com>
2024-10-30 12:52:39 -0300

    Add tests for delete and get users

--- src/test/java/br/com/hadryan/finance/stepdefs/UserStep.java
+++ src/test/java/br/com/hadryan/finance/stepdefs/UserStep.java
@@ -1,6 +1,5 @@
 package br.com.hadryan.finance.stepdefs;
 
-import br.com.hadryan.finance.exception.EmailAlreadyRegisteredException;
 import br.com.hadryan.finance.mapper.UserMapperImpl;
 import br.com.hadryan.finance.mapper.dto.request.UserPostRequest;
 import br.com.hadryan.finance.mapper.dto.response.UserGetResponse;
@@ -17,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -37,7 +37,6 @@ public class UserStep extends StepDefsDefault {
 
     private ResponseEntity<UserPostResponse> postResponse;
     private ResponseEntity<UserGetResponse> getResponse;
-    private String errorMessage;
 
     @Given("que eu ainda não sou cadastrado")
     public void queEuAindaNaoSouCadastrado() {
@@ -46,6 +45,21 @@ public class UserStep extends StepDefsDefault {
         user = userRepository.findByUsername("John");
 
         Assertions.assertThat(user).isEqualTo(Optional.empty());
+    }
+
+    @Given("que eu sou um usuário cadastrado")
+    public void queEuJaSouCadastrado() {
+        var user = userRepository.findByUsername("John");
+        if (user.isEmpty()) {
+            var newUser = new User();
+            newUser.setUsername("John");
+            newUser.setEmail("john.doe@email.com");
+            newUser.setPassword(passwordEncoder.encode("123456"));
+            newUser.setCreatedAt(LocalDateTime.now());
+            userRepository.save(newUser);
+        }
+
+        Assertions.assertThat(userRepository.findByEmail("john.doe@email.com")).isNotEmpty();
     }
 
     @And("já existe um usuário cadastrado com o email {string}")
@@ -72,9 +86,41 @@ public class UserStep extends StepDefsDefault {
 
     }
 
-    @Then("a resposta deve ter o status code {int}")
-    public void aRespostaDeveTerOStatusCode(int statusCode) {
+    @When("faço uma requisição GET para trazer os dados do usuário existente")
+    public void facoUmaRequisicaoGETParaTrazerOsDadosDoUsuarioExistente() {
+        var id = userRepository.findByUsername("John").get().getId();
+        getResponse = restTemplate.getForEntity(URL + "/" + id, UserGetResponse.class);
+    }
+
+    @When("faço uma requisição GET para trazer os dados do usuário inexistente")
+    public void facoUmaRequisicaoGETParaTrazerOsDadosDoUsuarioInexistente() {
+        getResponse = restTemplate.getForEntity(URL + "/0", UserGetResponse.class);
+    }
+
+    @When("faço uma requisição DELETE para deletar o usuário existente")
+    public void facoUmaRequisicaoDELETEParaTrazerOsDadosDoUsuarioExistente() {
+        var id = userRepository.findByUsername("John").get().getId();
+        getResponse = restTemplate.exchange(URL + "/" + id, HttpMethod.DELETE, null, UserGetResponse.class);
+    }
+
+    @When("faço uma requisição DELETE para deletar o usuário inexistente")
+    public void facoUmaRequisicaoDELETEParaTrazerOsDadosDoUsuarioInexistente() {
+        getResponse = restTemplate.exchange(URL + "/0", HttpMethod.DELETE, null, UserGetResponse.class);
+    }
+
+    @Then("a resposta da requisição POST deve ter o status code {int}")
+    public void aRespostaDaRequisicaoPostDeveTerOStatusCode(int statusCode) {
         Assertions.assertThat(postResponse.getStatusCode().value()).isEqualTo(statusCode);
+    }
+
+    @Then("a resposta da requisição GET deve ter o status code {int}")
+    public void aRespostaDaRequisicaoGetDeveTerOStatusCode(int statusCode) {
+        Assertions.assertThat(getResponse.getStatusCode().value()).isEqualTo(statusCode);
+    }
+
+    @Then("a resposta da requisição DELETE deve ter o status code {int}")
+    public void aRespostaDaRequisicaoDELETEDeveTerOStatusCode(int statusCode) {
+        Assertions.assertThat(getResponse.getStatusCode().value()).isEqualTo(statusCode);
     }
 
     @And("o usuário deve ser retornado com os dados corretos")
@@ -82,5 +128,19 @@ public class UserStep extends StepDefsDefault {
         var user = userRepository.findByUsername("John").get();
         Assertions.assertThat(postResponse.getBody().getUsername()).isEqualTo(user.getUsername());
         Assertions.assertThat(postResponse.getBody().getEmail()).isEqualTo(user.getEmail());
+
+    }
+
+    @Then("os dados retornados devem estar corretos")
+    public void osDadosRetornadosDevemEstarCorretos() {
+        var user = userRepository.findByUsername("John").get();
+        Assertions.assertThat(getResponse.getBody().getUsername()).isEqualTo(user.getUsername());
+        Assertions.assertThat(getResponse.getBody().getEmail()).isEqualTo(user.getEmail());
+    }
+
+    @And("o usuário deve ser deletado do banco de dados")
+    public void oUsuarioDeveSerDeletadoDoBancoDeDados() {
+        var user = userRepository.findByUsername("John");
+        Assertions.assertThat(user).isEqualTo(Optional.empty());
     }
 }
--- src/test/resources/features/user.feature
+++ src/test/resources/features/user.feature
@@ -5,7 +5,7 @@ Feature: Cadastro de usuário
     When faço uma requisição POST para cadastrar um usuario com os seguintes dados:
       | username | email          | password |
       | John  | john.doe@email.com| 123456   |
-    Then a resposta deve ter o status code 201
+    Then a resposta da requisição POST deve ter o status code 201
     And o usuário deve ser retornado com os dados corretos
 
   Scenario: Cadastrar usuário com email já cadastrado
@@ -14,11 +14,33 @@ Feature: Cadastro de usuário
     When faço uma requisição POST para cadastrar um usuario com os seguintes dados:
       | username | email          | password |
       | John  | john.doe@email.com| 123456   |
-    Then a resposta deve ter o status code 400
+    Then a resposta da requisição POST deve ter o status code 400
 
   Scenario: Cadastrar usuário com email inválido
     Given que eu ainda não sou cadastrado
     When faço uma requisição POST para cadastrar um usuario com os seguintes dados:
       | username | email          | password |
       | John  | john.doeemail.com| 123456   |
-    Then a resposta deve ter o status code 400
+    Then a resposta da requisição POST deve ter o status code 400
+
+  Scenario: Trazer os dados de um usuário
+    Given que eu sou um usuário cadastrado
+    When faço uma requisição GET para trazer os dados do usuário existente
+    Then a resposta da requisição GET deve ter o status code 200
+    And os dados retornados devem estar corretos
+
+  Scenario: Buscar usuário inexistente
+    Given que eu ainda não sou cadastrado
+    When faço uma requisição GET para trazer os dados do usuário inexistente
+    Then a resposta da requisição GET deve ter o status code 404
+
+  Scenario: Deletar usuário
+    Given que eu sou um usuário cadastrado
+    When faço uma requisição DELETE para deletar o usuário existente
+    Then a resposta da requisição DELETE deve ter o status code 204
+    And o usuário deve ser deletado do banco de dados
+
+  Scenario: Deletar usuário inexistente
+    Given que eu ainda não sou cadastrado
+    When faço uma requisição DELETE para deletar o usuário inexistente
+    Then a resposta da requisição DELETE deve ter o status code 404
