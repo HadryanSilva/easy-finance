bc1afff96084b2ef53df473cd8870440db504cdf
Hadryan Silva <hadryan2001@gmail.com>
2024-10-30 01:53:11 -0300

    Adjust exceptions and tests

--- src/main/java/br/com/hadryan/finance/exception/GlobalErrorHandler.java
+++ src/main/java/br/com/hadryan/finance/exception/GlobalErrorHandler.java
@@ -11,19 +11,19 @@ public class GlobalErrorHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<DefaultErrorMessage> handleNotFoundException(NotFoundException e) {
-        var errorResponse = new DefaultErrorMessage(HttpStatus.NOT_FOUND, e.getReason());
+        var errorResponse = new DefaultErrorMessage(HttpStatus.NOT_FOUND.value(), e.getReason());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     @ExceptionHandler(EmailAlreadyRegisteredException.class)
     public ResponseEntity<DefaultErrorMessage> handleEmailAlreadyRegisteredException(EmailAlreadyRegisteredException e) {
-        var errorResponse = new DefaultErrorMessage(HttpStatus.BAD_REQUEST, e.getReason());
+        var errorResponse = new DefaultErrorMessage(HttpStatus.BAD_REQUEST.value(), e.getReason());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<DefaultErrorMessage> handleConstraintViolationException(ConstraintViolationException e) {
-        var errorResponse = new DefaultErrorMessage(HttpStatus.BAD_REQUEST, e.getMessage());
+        var errorResponse = new DefaultErrorMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
